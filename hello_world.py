91fa09973811133b81dfa96db2da6a781eeb95b9
crossxwill <chiuwilliam@gmail.com>
2023-06-28 05:23:42 +0000

    swapped pandas with modin.pandas

--- hello_world.py
+++ hello_world.py
@@ -1,24 +1,36 @@
 # %% import packages
 import numpy as np
-import pandas as pd
+import modin.pandas as pd
 import statsmodels.api as stat
 import matplotlib.pyplot as plt
+import ray
+
+ray.shutdown()
+
+ray.init(runtime_env={'env_vars': {'__MODIN_AUTOIMPORT_PANDAS__': '1'}},
+         include_dashboard=False)
 
 # %% simulate a data frame with 3 columns: x1, x2, y.
 x1 = np.random.randn(100)
 x2 = np.random.randn(100)
 y = 2*x1 + 3*x2 + 4 + np.random.normal(0,4,100)
 
-# %% fit a linear model called "lin_mod"
-lin_mod = stat.OLS(y, stat.add_constant(np.column_stack((x1, x2))))
+my_df = pd.DataFrame({'x1': x1, 'x2': x2, 'y': y})
+
+# %% fit a linear model called "lin_mod" using my_df using the formula API
+lin_mod = stat.OLS.from_formula('y ~ x1 + x2', data=my_df._to_pandas())
 lin_mod_1 = lin_mod.fit()
 
 # %% summary of the model
 print(lin_mod_1.summary())
 
-# %% plot the residuals of lin_mod
-plt.scatter(range(len(lin_mod_1.resid)), lin_mod_1.resid)
+# %% plot the residuals of lin_mod against the prediction
+plt.scatter(lin_mod_1.predict(), lin_mod_1.resid)
 plt.ylabel('Residuals')
-plt.xlabel('Observations')
+plt.xlabel('Predictions')
 plt.show()
+# %%
+
+
+ray.shutdown()
 # %%
